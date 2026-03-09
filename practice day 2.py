c77559f3fb1d918328e1591582d1720a6518406b
Fatima Faheem <118333147+fatima064@users.noreply.github.com>
2026-03-09 09:21:44 +0000

    commit change

--- practice day 2.py
+++ practice day 2.py
@@ -7,3 +7,25 @@ if age >= 18 :
     print ("you can vote ")
 elif age <18 :
     print("you are not adult ,you cant vote for now ")
+
+#range
+numbers = range(5)
+print (numbers)
+
+# loops 
+#while loop
+
+i = 1
+while i <=10 :
+    print (i * "$")
+    i = i+1
+
+#for loop
+
+for i in range(5):
+     print(i + 1)
+
+#list (collection of items )
+marks = [95,94,98,97]
+print (marks)
+
