4136d16f3eb8bd49fdad6916140e937f81175f9e
Fatima Faheem <118333147+fatima064@users.noreply.github.com>
2026-03-11 09:59:56 +0000

    commit change

---
+++ practice day 3.py
@@ -0,0 +1,21 @@
+marks = [95,96,97,98,99,96,94]
+
+for score in marks :
+    print(score)
+
+
+marks.append(99)#to add something in start 
+
+print(marks)
+
+
+marks.insert(0, 99)
+
+print (93 in marks)
+
+
+#while loop 
+i = 0
+while i < len(marks):
+    print(marks[i])
+    i = i + 1
